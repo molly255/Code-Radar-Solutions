8672f469dae2deff298bd90acbd5f00fd5f99d3b
molly255 <mollly255@gmail.com>
2025-03-30 11:12:56 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -10,6 +11,7 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
@@ -17,15 +19,27 @@ void printArray(int arr[], int n) {
     printf("\n");
 }
 
-int main(){
+int main() {
     int n;
-    scanf("%d", &n);
-    int arr[n];
+    if (scanf("%d", &n) != 1 || n <= 0) {
+        printf("Invalid input! Enter a positive integer.\n");
+        return 1;
+    }
+    int *arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+        if (scanf("%d", &arr[i]) != 1) {
+            printf("Invalid input! Enter integers only.\n");
+            free(arr);
+            return 1;
+        }
     }
     bubbleSort(arr, n);
     printArray(arr, n);
+    free(arr);
 
     return 0;
 }
